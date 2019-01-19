2886a86e2b29a4cd893663f6ee1095429bafdceb
jiazhu <1027158353@qq.com>
2019-01-20 00:18:48 +0800

    使用requests方式进行获取页面数据

--- crawler.py
+++ crawler.py
@@ -1,8 +1,5 @@
 import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
+from lxml import etree
 
 CHANNEL_MAP = {
     'KNOWLEDGE': 'category_10',
@@ -19,15 +16,41 @@ CHANNEL_MAP = {
 }
 
 
+# https://www.pearvideo.com/category_loading.jsp?reqType=5&categoryId=6&start=0
+
 class PearCrawler:
     def __init__(self):
         self.base_url = 'https://www.pearvideo.com/'
-        self.browser = webdriver.Chrome()
         self.headers = {
             'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
         }
 
     pass
 
+    def get_html(self, category_id, start):
+        params = {
+            'reqType': '5',
+            'categoryId': category_id,
+            'start': start
+        }
+        item = {}
+
+        response = requests.get('https://www.pearvideo.com/category_loading.jsp?', params=params, headers=self.headers)
+        # print(response.text)
+
+        tree = etree.HTML(response.content.decode("utf-8"))
+        names = tree.xpath('/html/body/li/div/a/div[2]/text()')
+        urls = tree.xpath('/html/body/li/div/a/@href')
+        images = tree.xpath('/html/body/li/div/a/div[1]/div[1]/div/@style')
+
+        for name, url, img in zip(names, urls, images):
+            print(name)
+            print(self.base_url + url)
+            print(img[img.find('(') + 1:img.find(')')])
+
+        pass
 
 
+if __name__ == '__main__':
+    crawler = PearCrawler()
+    crawler.get_html(6, 0)
